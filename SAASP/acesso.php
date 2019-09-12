a71ca1a05a1e2b37eeba5dc833a11331d73bc346
TIEverton <evertonpinheiroti@hotmail.com>
2019-09-12 13:10:08 -0300

    Arquivos tela acesso

--- SAASP/acesso.php
+++ SAASP/acesso.php
@@ -1,37 +1,99 @@
-
 <!DOCTYPE html>
-<html lang="pt-br" >
-
+<html lang="en">
 <head>
-    
-  <meta charset="UTF-8">
   <title>SAASP - Acesso &copy;</title>
-  <script  src="js/jquery-3.3.1.min.js"></script>
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
-  <meta name="viewport" content="width=device-width, user-scalable=no">
-  <meta name="viewport" content="width=500, initial-scale=1">
-  <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-  <link rel="stylesheet" href="css/estilo.css">
-  <link rel="icon" type="imagem/png" href="img/icon.png">
-
-  
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+  <link rel="icon" type="image/png" href="img/icon.png"/>
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
-
 <body>
-<div class="form" style="height: 510px;">
-  <div class="thumbnail"><img src="img/icon.png"/></div>
-  <p id="login">Acessar</p>
-  <form class="login-form" method="post" action="php/verificar.php">
-    <input type="text" name="login" required name=nome placeholder="Digite seu nome de usuário..."/>
-    <input type="password" name="senha" required name=nome placeholder="Digite a sua senha..."/>
-    <input class="submit" type="submit" value="Entrar">
-    <p class="message">Não é registrado? <a href="cadastro.php">Crie uma conta!</a></p>
-  </form>
-</div>
   
-    <script  src="js/index.js"></script>
-   
-</body>
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="wrap-login100">
+        
 
+        <form class="login100-form validate-form" method="post" action="php/verificar.php">
+          <span class="login100-form-title">
+            <img src="img/icon.png" alt="IMG">
+            Acessar Sistema
+          </span>
+
+          <div class="wrap-input100 validate-input" data-validate = "E-mail válido é obrigatório: ex@abc.xyz">
+            <input class="input100" type="text" name="login" placeholder="Usuário">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+              <i class="fa fa-user" aria-hidden="true"></i>
+            </span>
+          </div>
+
+          <div class="wrap-input100 validate-input" data-validate = "Senha requerida">
+            <input class="input100" type="password" name="senha" placeholder="Senha">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+              <i class="fa fa-lock" aria-hidden="true"></i>
+            </span>
+          </div>
+          
+          <div class="container-login100-form-btn">
+            <button class="login100-form-btn" type="submit">
+              Entrar
+            </button>
+          </div>
+
+          <div class="text-center">
+            <span class="txt1">
+              Esqueceu seu
+            </span>
+            <a class="txt2" href="#">
+              Usuário / Senha?
+            </a>
+          </div>
+
+          <div class="text-center">
+            <a class="txt2" href="#">
+              Criar uma conta
+              <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  
+  
+
+  
+<!--===============================================================================================-->  
+  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/bootstrap/js/popper.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/tilt/tilt.jquery.min.js"></script>
+  <script >
+    $('.js-tilt').tilt({
+      scale: 1.1
+    })
+  </script>
+<!--===============================================================================================-->
+  <script src="js/main.js"></script>
+
+</body>
 </html>
